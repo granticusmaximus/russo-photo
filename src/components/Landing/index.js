867beeaaa8b62e56db0d7acc887f773f6dfdfb06
Grant Watson <gwatson117@gmail.com>
2020-02-12 13:58:12 -0700

    update to ui

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import main from '../../assets/img/mainlogo.png'
 
 const Landing = () => (
-  <div className="container-body">
-    <center><h1>Welcome Home</h1></center>
-    <center><img src={main} alt={"No Access"} height="50%" width="50%" /> </center>
+  <div className="container">
+    <center><h1 className="header">Welcome Home</h1></center>
+    <div className="container-body">
+      <center><img src={main} alt={"No Access"} height="50%" width="50%" /> </center>
+    </div>
+
   </div>
 );
 
